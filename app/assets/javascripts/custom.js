6cf41f1a292c4a624fc7b3296bc1dca63739208c
abineko <yukiuematsu@icloud.com>
2019-08-28 21:32:32 +0000

    top画面スマホ目処

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,26 @@
 $(function () {
-  $('.slideshow').slick({
-    autoplay:true,
-    autoplaySpeed:3000,
-    dots:true
-  });
+var mql = window.matchMedia('screen and (max-width: 599px)');
+  function checkBreakPoint(mql) {
+    if (mql.matches) {
+      // スマホ向け（599px以下のとき）
+      $('.slideshow.slick-initialized').slick('unslick');
+      
+    } else {
+      // PC向け
+     
+       $('.slideshow').not('.slick-initialized').slick({
+        //スライドさせる
+        autoplay:true,
+        autoplaySpeed:3000,
+        arrows: true,
+        dots: true
+      });
+    }
+  }
+
+  // ブレイクポイントの瞬間に発火
+  mql.addListener(checkBreakPoint);
+
+  // 初回チェック
+  checkBreakPoint(mql);
 });
