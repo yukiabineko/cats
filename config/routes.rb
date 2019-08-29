c1c151870264bc85402d871c1b351fd7237dbc92
abineko <yukiuematsu@icloud.com>
2019-08-29 22:20:35 +0000

    新規登録画面大枠

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   
   root "tops#show"                                                   #root
   get '/signup',to: 'users#new'                                      #会員登録
+  
+  resources :users                                                   #ユーザーカラム一式
 
   get    '/login', to: 'sessions#new'                                #ログインページへ                       
   post   '/login', to: 'sessions#create'                             #ログイン処理
-  delete '/logout', to: 'sessions#destroy'                          #ログアウト処理
+  delete '/logout', to: 'sessions#destroy'                           #ログアウト処理
 end
 
