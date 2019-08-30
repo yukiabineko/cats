273bae8f1af707132f79c37a3a3b0fd94e5e17fe
abineko <yukiuematsu@icloud.com>
2019-08-30 07:31:40 +0000

    新規会員微調整

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20190828214524) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.boolean "admin"
+    t.boolean "admin",default:false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
