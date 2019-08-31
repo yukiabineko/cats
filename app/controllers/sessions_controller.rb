df7a1f4fa3549c49f50c383fcb048c4b2fa83fe2
abineko <yukiuematsu@icloud.com>
2019-08-31 22:17:02 +0000

    ログイン状態によるheader分岐

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
-  def new
+  
+  def new  #ログインページ
+  end
+  
+  def create  #ログイン機能
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user 
+    else
+      flash.now[:warning] = 'メールアドレスまたはパスワードが間違ってます。'
+      render :new
+    end
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+    
+  def log_in(user)           #session id格納
+    session[:user_id] = user.id
+  end    
+  
+  def current_user          #ログインユーザ定義
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+  
+  def logged_in?            #ログインしているか?(current_userがなければfalse)
+    !current_user.nil?
+  end
+  
 end
