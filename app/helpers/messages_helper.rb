5057533ca599ac2ad3c78aecc09b9df719384d3e
abineko <yukiuematsu@icloud.com>
2019-09-08 04:43:56 +0000

    reply js

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,5 +1,9 @@
 module MessagesHelper
     def replay_count(message)
         return message.replies.all.count
-    end        
+    end
+    
+    def replies(message)
+        return message.replies.all
+    end
 end
