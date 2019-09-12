92530d2c8c41244b62d5ccf6341e4e366e57d0dd
abineko <yukiuematsu@icloud.com>
2019-09-12 06:13:01 +0000

    picture slide custom

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,26 +1,6 @@
 $(function () {
-var mql = window.matchMedia('screen and (max-width: 599px)');
-  function checkBreakPoint(mql) {
-    if (mql.matches) {
-      // スマホ向け（599px以下のとき）
-      $('.slideshow.slick-initialized').slick('unslick');
-      
-    } else {
-      // PC向け
-     
-       $('.slideshow').not('.slick-initialized').slick({
-        //スライドさせる
-        autoplay:true,
-        autoplaySpeed:3000,
-        arrows: true,
-        dots: true
-      });
-    }
-  }
-
-  // ブレイクポイントの瞬間に発火
-  mql.addListener(checkBreakPoint);
-
-  // 初回チェック
-  checkBreakPoint(mql);
+　$('.slideshow').slick({
+　   autoplay:true,
+　   autoplaySpeed:3000,
+　});
 });
