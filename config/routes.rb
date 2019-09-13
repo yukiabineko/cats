e465c29f0670755aa87c7b4f181687045af42dcb
abineko <yukiuematsu@icloud.com>
2019-09-13 00:40:19 +0000

    check g2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  get 'auth/provider/callback', to: 'sessions#create2'
+  get 'auth/:provider/callback', to: 'sessions#create2'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy2', as: 'signout'
   get '/auth/:provider/callback',    to: 'users#facebook_login',      as: :auth_callback
