bc50967c097439cc0262fe74bcece98325e306da
abineko <yukiuematsu@icloud.com>
2019-09-13 17:05:37 +0000

    スケジュール日validation

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,16 @@ class Schedule < ApplicationRecord
   belongs_to :cat
   validates :plan_date,  presence: true
   validates :plan_content,  presence: true
+  validate :over_day
+  
+  
+  
+  
+  def over_day
+    if self.plan_date.present?
+      if self.plan_date < Date.today
+         errors.add(:plan_date, "日付がすぎてます") 
+      end   
+    end    
+  end    
 end
