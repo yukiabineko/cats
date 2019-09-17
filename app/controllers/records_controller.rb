0447cadb85979275e35c0b92dd58025a5e36a3b1
abineko <yukiuematsu@icloud.com>
2019-09-17 05:11:32 +0000

    健康チェック

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -15,6 +15,7 @@ class RecordsController < ApplicationController
   end
 #------------------------------------------------------------------------------------------------
   def result_view
+     @all_cats =current_user.cats.all if current_user.present? #ユーザー猫総数
     #体重結果ページ更新ボタン押された場合の処理
     if request.get? && logged_in? 
       redirect_to record_url
@@ -22,77 +23,99 @@ class RecordsController < ApplicationController
     if logged_in?   
     #猫複数形パラメーター
     @obj ={}
-    if params[:records]
-      cats_multiple_parameter.each do |id,item|
-        @cats={}
-        cat = Cat.find id
-        #１歳以上の猫        
-        if cat.cat_age >1.2
-          obj= Base.find_by(data_age: 1.2)
-        #1才未満          
-        elsif cat.cat_age <=1.0
-       #ここが複雑        
-          obj= Base.find_by(data_age: params[:records][id][:cat_age])
+      if params[:records]
+        cats_multiple_parameter.each do |id,item|
+          @cats={}
+          cat = Cat.find id
+          #１歳以上の猫        
+          if cat.cat_age >1.2
+            obj= Base.find_by(data_age: 1.2)
+          #1才未満          
+          elsif cat.cat_age <=1.0
+         #ここが複雑        
+            obj= Base.find_by(data_age: params[:records][id][:cat_age])
+          end
+          min_weight = obj.min_weight
+            max_weight = obj.max_weight
+            if cat.cat_weight < min_weight
+              str = "痩せてます"
+            elsif  cat.cat_weight >max_weight
+              str = "太ってます"
+            elsif cat.cat_weight >= min_weight && cat.cat_weight<= max_weight
+              str = "標準です"    
+            end
+          @cats[:id] = cat.id
+          @cats[:date] = Date.today
+          @cats[:name] = cat.cat_name
+          @cats[:weight] = cat.cat_weight
+          @cats[:data_weight] = "#{min_weight}~#{max_weight}"
+          @cats[:result] = str
+          @obj[id]=@cats  #各猫配列をさらに配列格納
+        end    
+      #猫単数パラメーター      
+      elsif params[:name]
+        @cat = Cat.find(params[:id])
+        @date = Date.today
+        #1才以上の猫      
+        if @cat.cat_age > 1.2
+          obj = Base.find_by(data_age: 1.2)
+        elsif @cat.cat_age <= 1.0 
+          obj = Base.find_by(data_age: params[:age])  
         end
         min_weight = obj.min_weight
-          max_weight = obj.max_weight
-          if cat.cat_weight < min_weight
-            str = "痩せてます"
-          elsif  cat.cat_weight >max_weight
-            str = "太ってます"
-          elsif cat.cat_weight >= min_weight && cat.cat_weight<= max_weight
-            str = "標準です"    
-          end
-        @cats[:id] = cat.id
-        @cats[:date] = Date.today
-        @cats[:name] = cat.cat_name
-        @cats[:weight] = cat.cat_weight
-        @cats[:data_weight] = "#{min_weight}~#{max_weight}"
-        @cats[:result] = str
-        @obj[id]=@cats  #各猫配列をさらに配列格納
-      end    
-    #猫単数パラメーター      
-    elsif params[:name]
-      @cat = Cat.find(params[:id])
-      @date = Date.today
-      #1才以上の猫      
-      if @cat.cat_age > 1.2
-        obj = Base.find_by(data_age: 1.2)
-      elsif @cat.cat_age <= 1.0 
-        obj = Base.find_by(data_age: params[:age])  
-      end
-      min_weight = obj.min_weight
-      max_weight = obj.max_weight
-      @data_weight = "#{min_weight}~#{max_weight}"
-      if @cat.cat_weight < min_weight
-        @result = "痩せてます"
-      elsif  @cat.cat_weight > max_weight
-        @result ="太ってます"
-      else
-          @result = "標準です"    
-      end    
-    end 
-#未ログイン    
+        max_weight = obj.max_weight
+        @data_weight = "#{min_weight}~#{max_weight}"
+        if @cat.cat_weight < min_weight
+          @result = "痩せてます"
+        elsif  @cat.cat_weight > max_weight
+          @result ="太ってます"
+        else
+            @result = "標準です"    
+        end
+      #登録猫なしの時(login済み)      
+      elsif @all_cats.count == 0
+          if params[:cat_weight].present?
+            @date =Date.today 
+            @name = "#{current_user.name}さんの猫"
+            obj = Base.find_by(data_age: params[:cat_age])
+            min_weight = obj.min_weight
+            max_weight = obj.max_weight
+            @data_weight = "#{min_weight}~#{max_weight}"
+            if params[:cat_weight].to_f < min_weight
+              @result = "痩せてます"  
+            elsif params[:cat_weight].to_f > max_weight  
+              @result ="太ってます"
+            else  
+              @result = "標準です" 
+            end  
+          #体重未入力の処理  
+          else
+            flash[:danger] = "体重を入力してください"
+          end    
+      
+      end 
+  #-------------------------------------------------------------    
+  #未ログイン  
     else
-     if params[:cat_weight].present?
-      @date =Date.today
-      @name ="ゲストさんの猫ちゃん"
-      obj = Base.find_by(data_age: params[:cat_age])
-      min_weight = obj.min_weight
-      max_weight = obj.max_weight
-      @data_weight = "#{min_weight}~#{max_weight}"
-      if params[:cat_weight].to_f < min_weight
-        @result = "痩せてます"
-      elsif params[:cat_weight].to_f > max_weight
-        @result ="太ってます"
-      else
-          @result = "標準です"    
-      end   
-    #体重未入力の処理
-     else
-      flash[:danger] = "体重を入力してください"
-      redirect_to record_url
-     end  
+       if params[:cat_weight].present?
+        @date =Date.today
+        @name ="ゲストさんの猫ちゃん"
+        obj = Base.find_by(data_age: params[:cat_age])
+        min_weight = obj.min_weight
+        max_weight = obj.max_weight
+        @data_weight = "#{min_weight}~#{max_weight}"
+        if params[:cat_weight].to_f < min_weight
+          @result = "痩せてます"
+        elsif params[:cat_weight].to_f > max_weight
+          @result ="太ってます"
+        else
+            @result = "標準です"    
+        end   
+      #体重未入力の処理
+       else
+        flash[:danger] = "体重を入力してください"
+        redirect_to record_url
+       end  
     end  
   end
 #========================================================  
