deab714ae77bf37006aaac55c554f38d60251962
abineko <yukiuematsu@icloud.com>
2019-10-04 08:48:38 +0000

    CSS

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :messages,dependent: :destroy
-  validates :post_title,  presence: true,length: { maximum: 30 } 
+  validates :post_title,  presence: true,length: { maximum: 12 } 
   validates :post_content,  presence: true
 end
