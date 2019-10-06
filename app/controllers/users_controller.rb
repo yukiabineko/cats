72d890376940fcc5ceec754b7bb4445ca6c33970
abineko <yukiuematsu@icloud.com>
2019-10-06 14:32:05 +0000

    CSS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -125,12 +125,12 @@ end
   end
  #画像バイナリー表示オーナー 
   def show_image  
-    send_data @user.image,:type => 'image/jpg'
+    send_data @user.image,:type => 'image/jpeg'
   end
  #画像バイナリー表示猫
   def cat_show_image  
      @cat = Cat.find(params[:id])
-    send_data @cat.cat_image,:type => 'image/jpg'
+    send_data @cat.cat_image,:type => 'image/jpeg'
   end  
 
   def facebook_login
