c466eeb3ee7e10e533b54408137477d7f2649549
abineko <yukiuematsu@icloud.com>
2019-10-08 01:34:32 +0000

    CSS

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -15,3 +15,22 @@
         }
     }
 });
+     var obj = document.getElementsByClassName('over_link');
+      var num =0;
+      colorChange();
+    function colorChange(){
+     for(var i=0; i<obj.length; i++){
+         num +=1
+    if(num %2 == 0){
+        obj[i].style.color = "#ff3333";
+       
+    }
+    else{
+         obj[i].style.color = "#990000";
+         
+    }
+    setTimeout("colorChange()",50);
+     }     
+    
+}
+ 
